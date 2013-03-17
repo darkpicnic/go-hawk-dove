6560639335b207a9033eafd0bef699040cc532e8
Sebastian <darkpicnic@gmail.com>
2013-03-17 03:09:46 -0400

    Broken

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 const (
 	Active = "active"
@@ -10,10 +14,18 @@ const (
 )
 
 const (
-	StartingDoves  = 100
-	StartingHawks  = 5
-	StartingEnergy = 100
-	Rounds         = 100
+	StartingDoves                 = 100
+	StartingHawks                 = 5
+	StartingEnergy                = 100
+	Rounds                        = 100
+	EnergyLossPerRound            = 2
+	EnergyLossFromFighting        = 200
+	EnergyRequiredForLiving       = 20
+	EnergyLostFromBluffing        = 10
+	EnergyRequiredForReproduction = 250
+
+	MinFoodPerRound = 20
+	MaxFoodPerRound = 70
 )
 
 var agents []*Agent
@@ -25,11 +37,27 @@ type Agent struct {
 }
 
 func main() {
+	currentRound := 1
+	for currentRound < Rounds {
+		awakenAgents()
+
+		for {
+			agent, nemesis := getRandomAgents()
+			compete(agent, nemesis, createFood())
+		}
+
+		atrophyAgents()
+
+		currentRound += 1
+	}
 	fmt.Printf("Total agents: %d", len(agents))
 }
 
 // Init creates the initial doves and hawks
 func init() {
+
+	rand.Seed(time.Now().UTC().UnixNano())
+
 	doveCount := 1
 	for doveCount < StartingDoves {
 		d := new(Agent)
@@ -49,4 +77,28 @@ func init() {
 		agents = append(agents, d)
 		hawkCount += 1
 	}
+}
+
+func atrophyAgents() {
+	for _, agent := range agents {
+		agent.Energy -= EnergyLossPerRound
+	}
+}
+
+func createFood() int {
+	return MinFoodPerRound + rand.Intn(MaxFoodPerRound)
+}
+
+func awakenAgents() {
+	for _, agent := range agents {
+		agent.Status = Active
+	}
+}
+
+func compete(agent Agent, nemesis Agent, food int) {
+	return
+}
+
+func getRandomAgents() (agent, nemesis Agent) {
+	return
 }
